2787aad5a9dd8d5eb41eef45de3566f06305f60a
ddongbae-i <viragomk@gmail.com>
2025-12-27 11:24:11 +0900

    11:24

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1744,13 +1744,13 @@ const IntroSection: React.FC = () => {
             <div
               className="absolute"
               style={{
-                width: "200vw",
-                height: "200vh",
+                width: "98vw",
+                height: "110vh",
                 background: BEAM_COLOR,
-                transform: "rotate(-39deg)",
+                transform: "rotate(-30deg)",
                 transformOrigin: "top right",
-                top: "-50vh",
-                right: "-20vw",
+                top: "0vh",
+                right: "20vw",
               }}
             />
           </motion.div>
@@ -2392,13 +2392,17 @@ const IntroSection: React.FC = () => {
             height: "700px",
             left: "50%",
             x: "-50%",
-            top: "-6%",
+            // top: "-6%",
             transformStyle: "preserve-3d",
             zIndex: 100,
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
           }}
+          animate={{
+            top: phase >= 21 ? "-10%" : "-6%",  // ✅ 모자와 맞춰서 이동
+          }}
+          transition={{ duration: 0.6, ease: "backOut" }}
         >
           {/* 내부 wrapper - 얼굴만, 크기 조절 */}
           <motion.div
